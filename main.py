c15a135a8a40ced024870df5bfccc92f7b1b0afb
Dandi Septiandi <dandi.septiandi2001@gmail.com>
2022-09-18 20:51:43 +0700

    add root feature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from math_function import add, mul, div, exp, max_number
-import math
+from math_function import add, mul, div, exp, max_number, root
 
 def main():
     
@@ -28,8 +27,8 @@ def main():
         print("max number is", result)
 
     if operator == "root":
-        data_1 = int(input("masukkan input 1 :"))
-        print(math.sqrt(data_1,))
+        result = root(data_1, data_2)
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
 if __name__ == "__main__":
     print("Hello Main !")
--- math_function.py
+++ math_function.py
@@ -14,3 +14,6 @@ def exp(a, b):
 
 def max_number(a, b):
     return max(a, b)
+
+def root(a, b):
+    return a*(math.sqrt(b))
